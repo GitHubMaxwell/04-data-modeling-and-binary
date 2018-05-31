3c4560c7e91545aae8308983c0736d8e19d8aab4
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-05-30 18:32:11 -0700

    added test to make a new file with a transformation applied to it

--- __test__/file.test.js
+++ __test__/file.test.js
@@ -1,1 +1,42 @@
+const bitmap = require('../src/lib/bitmap.js');
+const fileReader = require('../src/lib/file.js');
+//none of these tests are real for file.test.js
 
+describe('FILE Module', () => {
+  it('tests to see undefined with non existent file', (done)=>{
+    const expected = undefined;
+    fileReader.readFile(`${__dirname}/../../assets/missing.bmp`,(err) =>{
+      expect(expected).toBeUndefined();
+      done();
+    });
+  });
+
+  //   it('tests to see undefined with non existent file as second file', (done)=>{
+  //     const expected = undefined;
+  //     fileReader.readFile(`${__dirname}/../../assets/bitmap.bmp`, 'missing.txt',(err) =>{
+  //       expect(expected).toBeUndefined();
+  //       done();
+  //     });
+  //   });
+
+  //   it('tests to see undefined (hardcoded to bitmap.js) is being returned', (done)=>{
+  //     // const expected = `${__dirname}/../../assets/bitmap.bmp`;
+  //     fileReader.readFile(`${__dirname}/../../assets/bitmap.bmp`,(err,data)=>{
+  //       expect(buffer(data)).toBeNull();
+  //       done();
+  //     });
+  //   });
+  
+//   it('tests to see if the data from file.js is being passed and parsed in bitmap.js', (done)=>{
+//     // const expected = `${__dirname}/../../assets/bitmap.bmp`;
+//     fileReader.readFile(`${__dirname}/../assets/bitmap.bmp`,(err,data)=>{
+//     //   console.log(`${__dirname}/../assets/bitmap.bmp`);
+//     //   console.log(data);
+//       // expect(bitmap(data).toString('utf-8', 0, 2)).toBe('BM');
+//       console.log((bitmap(data)).type);
+//       expect((bitmap(data)).type).toBe('BM');
+//       done();
+//     });
+//   });
+
+});
--- src/lib/file.js
+++ src/lib/file.js
@@ -14,6 +14,7 @@ exports.readFile = (path, callback) => {
 exports.writeFile = (fileName, newBuffer, callback) => {
   fs.writeFile(fileName, newBuffer, (err, data) => {
     if (err) return callback(err);
+    console.log('New file written');
     return callback(null, data);
   });
 };
